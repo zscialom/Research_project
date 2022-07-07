a72ff58140fb427613eacfc8e220861e80204e9a
Scialom Zachary <zscialom@enseirb-matmeca.fr>
2022-07-07 13:12:39 +0200

    Trading started &Adjustments made

--- financial_models_testing.m
+++ financial_models_testing.m
@@ -1,38 +1,38 @@
 clc
-clear all
+clear 
 close all
 
 %% Parameters
 
-rf = 0.00153;
-T = 150/250;
+rf = 0.0168;
+T = 144/250;
 
-S0_CSCO = 43.32;
+S0_CSCO = 41.11;
 K_CSCO = 55;
 histo_vol_CSCO = 0.3023
-implied_vol_CSCO = 0.0625;
+implied_vol_CSCO = 0.254;
 q_CSCO = 0.0152;
 
 
-S0_GE = 65.29;
-K_GE = 10;
+S0_GE = 61.23;
+K_GE = 35;
 histo_vol_GE = 0.3305
-implied_vol_GE = 1.64;
+implied_vol_GE = 0.41;
 q_GE = 0.0032;
 
 %% Black-Scholes-Merton
 
-[Call_CSCO, Put_CSCO] = blsprice(S0_CSCO, K_CSCO, rf, T, histo_vol_CSCO,q_CSCO)
+%[Call_CSCO, Put_CSCO] = blsprice(S0_CSCO, K_CSCO, rf, T, histo_vol_CSCO,q_CSCO)
 [Call_CSCO_implied_vol, Put_CSCO_implied_vol] = blsprice(S0_CSCO, K_CSCO, rf, T, implied_vol_CSCO,q_CSCO)
 
-[CallDelta_CSCO,PutDelta_CSCO] = blsdelta(S0_CSCO, K_CSCO, rf, T, histo_vol_CSCO,q_CSCO)
-Gamma_CSCO = blsgamma(S0_CSCO, K_CSCO, rf, T, histo_vol_CSCO,q_CSCO)
-Vega_CSCO = blsvega(S0_CSCO, K_CSCO, rf, T, histo_vol_CSCO,q_CSCO)
+[CallDelta_CSCO,PutDelta_CSCO] = blsdelta(S0_CSCO, K_CSCO, rf, T, implied_vol_CSCO,q_CSCO)
+Gamma_CSCO = blsgamma(S0_CSCO, K_CSCO, rf, T, implied_vol_CSCO,q_CSCO)
+Vega_CSCO = blsvega(S0_CSCO, K_CSCO, rf, T, implied_vol_CSCO,q_CSCO)
 
-[Call_GE, Put_GE] = blsprice(S0_GE, K_GE, rf, T, histo_vol_GE, q_GE)
+%[Call_GE, Put_GE] = blsprice(S0_GE, K_GE, rf, T, histo_vol_GE, q_GE)
 [Call_GE_implied_vol, Put_GE_implied_vol] = blsprice(S0_GE, K_GE, rf, T, implied_vol_GE, q_GE)
 
-[CallDelta_GE,PutDelta_GE] = blsdelta(S0_GE, K_GE, rf, T, histo_vol_GE, q_GE)
-Gamma_GE = blsgamma(S0_GE, K_GE, rf, T, histo_vol_GE, q_GE)
-Vega_GE = blsvega(S0_GE, K_GE, rf, T, histo_vol_GE, q_GE)
+[CallDelta_GE,PutDelta_GE] = blsdelta(S0_GE, K_GE, rf, T, implied_vol_GE, q_GE)
+Gamma_GE = blsgamma(S0_GE, K_GE, rf, T, implied_vol_GE, q_GE)
+Vega_GE = blsvega(S0_GE, K_GE, rf, T, implied_vol_GE, q_GE)
 
--- heston_model.m
+++ heston_model.m
@@ -1,31 +1,59 @@
 % MATLAb 2018a
 
 clc
-clear all
+clear 
 close all
 
-%% Parameters
+%% General Parameters
 
-AssetPrice = 80;
-Rate = 0.03;
-DividendYield = 0.02;
-OptSpec = 'call';
+rf = 0.0185;
+T = 143/250;
 
-V0 = 0.04;                  % initial variance of underlying asset
-ThetaV = 0.05;              % long-term variance of underlying asset
-Kappa = 1.0;                % mean-reversion speed for the variance of the underlying asset
-SigmaV = 0.2;               % volatility of the volatility
-RhoSV = -0.7;               % correlation between Wiener processes
+% CSCO
+S0_CSCO = 42.49;
+K_CSCO = 55;
+q_CSCO = 0.0152;
+
+% GE
+S0_GE = 61.19;
+K_GE = 35;
+q_GE = 0.0032;
+
+%% Heston parameters
+OptSpec_CSCO = 'call';
+OptSpec_GE = 'put';
+
+V0_CSCO = 0.1125;          % initial variance of underlying asset
+ThetaV_CSCO = 0.0268;            % long-term variance of underlying asset
+Kappa_CSCO = 1.6181;             % mean-reversion speed for the variance of the underlying asset
+SigmaV_CSCO = 0.3121;            % volatility of the volatility
+RhoSV_CSCO = -0.6;       % correlation between Wiener processes
+
+V0_GE = 0.3022;                % initial variance of underlying asset
+ThetaV_GE = 0.0108;            % long-term variance of underlying asset
+Kappa_GE = 0.1211;             % mean-reversion speed for the variance of the underlying asset
+SigmaV_GE = 0.0412;            % volatility of the volatility
+RhoSV_GE = 0.2;                % correlation between Wiener processes
+
 
 %% Call Derivation
 
-Settle = datenum('17-Jun-2022');
-Maturity = datemnth(Settle, 6);
-Strike = 80; 
+Settle = datenum('05-Jul-2022');
+Maturity = datenum('20-Jan-2023');
 
-Call_test = optByHestonNI(Rate, AssetPrice, Settle, Maturity, OptSpec, Strike, ...
-    V0, ThetaV, Kappa, SigmaV, RhoSV, 'DividendYield', DividendYield)
+Call_CSCO = optByHestonNI(rf, S0_CSCO, Settle, Maturity, OptSpec_CSCO, K_CSCO, ...
+    V0_CSCO, ThetaV_CSCO, Kappa_CSCO, SigmaV_CSCO, RhoSV_CSCO, 'DividendYield', q_CSCO)
 
-Delta_test = optSensByHestonNI(Rate, AssetPrice, Settle, Maturity, OptSpec, Strike, ...
-    V0, ThetaV, Kappa, SigmaV, RhoSV, 'DividendYield', DividendYield, 'OutSpec', 'delta')
+[Delta_CSC0, Gamma_CSCO, Vega_CSCO] = optSensByHestonNI(rf, S0_CSCO, Settle, Maturity, OptSpec_CSCO, K_CSCO, ...
+    V0_CSCO, ThetaV_CSCO, Kappa_CSCO, SigmaV_CSCO, RhoSV_CSCO, 'DividendYield', q_CSCO, 'OutSpec', ["delta", "gamma", "rho"])
+
+
+%% Put Derivation
+
+Put_GE = optByHestonNI(rf, S0_GE, Settle, Maturity, OptSpec_GE, K_GE, ...
+    V0_GE, ThetaV_GE, Kappa_GE, SigmaV_GE, RhoSV_GE, 'DividendYield', q_GE)
+
+[Delta_GE, Gamma_GE, Vega_GE] = optSensByHestonNI(rf, S0_GE, Settle, Maturity, OptSpec_GE, K_GE, ...
+    V0_GE, ThetaV_GE, Kappa_GE, SigmaV_GE, RhoSV_GE, 'DividendYield', q_GE, 'OutSpec', ["delta", "gamma", "rho"])
+
 
